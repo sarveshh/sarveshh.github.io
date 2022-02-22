93a7758d2a37142424b4aefb441d0afa8b235025
Sarvesh Patil <sarveshp273@gmail.com>
2022-02-22 13:09:54 +0530

    Added map in contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import classes from './Contact.module.css'
+import { MapContainer } from './MapContainer'
 
 const Contact = () => {
 	return (
-		<div>Contact</div>
+		<div className={classes.contactMe}>
+			<div className={classes.contactWrapper}>
+				<h1 className={classes.contactHeader}>Say hello</h1>
+				<div className={classes.contactForm}>
+
+				</div>
+			</div>
+			<div className={classes.contactMap}>
+				<MapContainer />
+				{/* AIzaSyAoSOST6nQjpi1Xn_bBIWJQCX6_jKASWPU */}
+			</div>
+		</div>
 	)
 }
 
