8fcb59191b5b22f211280d4c155927f2beba7095
Sarvesh Patil <sarveshp273@gmail.com>
2022-02-26 14:44:31 +0530

    Updated with useRef

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,93 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./Contact.css";
 import { MapContainer } from "./MapContainer";
 
 const Contact = () => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const messageInputRef = useRef();
 
-	useEffect(() => {
-		const inputs = document.querySelectorAll(".input");
+  useEffect(() => {
+    const inputs = document.querySelectorAll(".input");
 
-		function focusFunc() {
-			let parent = this.parentNode;
-			parent.classList.add("focus");
-		}
+    function focusFunc() {
+      let parent = this.parentNode;
+      parent.classList.add("focus");
+    }
 
-		function blurFunc() {
-			let parent = this.parentNode;
-			if (this.value == "") {
-				parent.classList.remove("focus");
-			}
-		}
-		inputs.forEach((input) => {
-			input.addEventListener("focus", focusFunc);
-			input.addEventListener("blur", blurFunc);
-		});
-	}, []);
+    function blurFunc() {
+      let parent = this.parentNode;
+      if (this.value === "") {
+        parent.classList.remove("focus");
+      }
+    }
+    inputs.forEach((input) => {
+      input.addEventListener("focus", focusFunc);
+      input.addEventListener("blur", blurFunc);
+    });
+  }, []);
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		alert(
-			`The name you entered was: ${name}, the email you entered was: ${email}, and the message you entered was: ${message}`
-		);
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      `The name you entered was: ${nameInputRef.current.value}, the email you entered was: ${emailInputRef.current.value}, and the message you entered was: ${messageInputRef.current.value}`
+    );
+  };
 
-	return (
-		<div className="container">
-			<div className="form">
-				<div className="contact-info">
-					<div className="info">
-						<MapContainer />
-					</div>
-				</div>
-				<div className="contact-form">
-					<span className="circle one"></span>
-					<span className="circle two"></span>
-					<form autoComplete="off" onSubmit={handleSubmit}>
-						<h3 className="title">Let's get in touch</h3>
-						<div className="input-container">
-							<input
-								autoComplete="none"
-								type="text"
-								name="name"
-								value={name}
-								className="input"
-								onChange={(e) => setName(e.target.value)}
-							/>
-							<label>Full Name</label>
-							<span>Full Name</span>
-						</div>
-						<div className="input-container">
-							<input
-								type="email"
-								value={email}
-								autoComplete="off"
-								name="email"
-								className="input"
-								onChange={(e) => setEmail(e.target.value)}
-							/>
-							<label>Email</label>
-							<span>Email</span>
-						</div>
-						<div className="input-container textarea">
-							<textarea
-								value={message}
-								onChange={(e) => setMessage(e.target.value)}
-								name="message"
-								autoComplete="off"
-								className="input"
-							></textarea>
-							<label>Message</label>
-							<span>Message</span>
-						</div>
-						<input type="submit" value="Send Message" className="btn" />
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="form">
+        <div className="contact-info">
+          <div className="info">
+            <MapContainer />
+          </div>
+        </div>
+        <div className="contact-form">
+          <span className="circle one"></span>
+          <span className="circle two"></span>
+          <form autoComplete="off" onSubmit={handleSubmit}>
+            <h3 className="title">Let's get in touch</h3>
+            <div className="input-container">
+              <input
+                autoComplete="none"
+                type="text"
+                name="name"
+                className="input"
+                ref={nameInputRef}
+              />
+              <label>Full Name</label>
+              <span>Full Name</span>
+            </div>
+            <div className="input-container">
+              <input
+                type="email"
+                autoComplete="off"
+                name="email"
+                className="input"
+                ref={emailInputRef}
+              />
+              <label>Email</label>
+              <span>Email</span>
+            </div>
+            <div className="input-container textarea">
+              <textarea
+                name="message"
+                autoComplete="off"
+                className="input"
+                ref={messageInputRef}
+              ></textarea>
+              <label>Message</label>
+              <span>Message</span>
+            </div>
+            <input type="submit" value="Send Message" className="btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
