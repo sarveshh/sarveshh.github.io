64d8a81cb9f6f2a4127f3b1afaa0302d3bdb9e14
Sarvesh Patil <sarveshp273@gmail.com>
2022-06-01 14:04:55 +0530

    Made project 100% css-free

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from "@mui/material/GlobalStyles";
+
+const globalStyle = {
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+};
 
 const app = (
   <BrowserRouter>
+    <GlobalStyles styles={globalStyle} />
+
     <App />
   </BrowserRouter>
 );
