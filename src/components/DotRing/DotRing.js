c6a68faf0bf53f025e76f1b8a346cfe7d3fe1301
Sarvesh Patil <sarveshp273@gmail.com>
2022-06-04 21:22:25 +0530

    csss to js custom cursor

--- src/components/DotRing/DotRing.js
+++ src/components/DotRing/DotRing.js
@@ -1,21 +1,15 @@
 import React, { useContext } from "react";
-import "./DotRing.css";
 import useMousePosition from "../../hooks/useMousePosition";
 import { MouseContext } from "../../context/mouse-context";
+import { DotDiv, RingDiv } from "./DotRingStyles";
 
 const DotRing = () => {
   const { cursorType, cursorChangeHandler } = useContext(MouseContext);
   const { x, y } = useMousePosition();
   return (
     <>
-      <div
-        style={{ left: `${x}px`, top: `${y}px` }}
-        className={"ring " + cursorType}
-      ></div>
-      <div
-        className={"dot " + cursorType}
-        style={{ left: `${x}px`, top: `${y}px` }}
-      ></div>
+      <RingDiv x={x} y={y} className={"ring " + cursorType}></RingDiv>
+      <DotDiv x={x} y={y} className={"dot " + cursorType}></DotDiv>
     </>
   );
 };
